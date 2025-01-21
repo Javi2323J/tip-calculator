938e09d094bb3ab51aa4888183d45c539a881506
DESKTOP-C6NF0QP\jrome <jromera232003@gmail.com>
2025-01-21 19:58:54 +0100

    create MenuItem component; add data file with products; add type for menu items #1

--- tip-calculator/src/App.tsx
+++ tip-calculator/src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 
+import { menuItems } from './data/db'
+
+import MenuItem from './components/MenuItem'
+
 function App(): React.JSX.Element {
   return (
     <>
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Tips and Consume Calculator</h1>
       </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+
+          {menuItems.map(item =>
+            <MenuItem
+              key={item.id}
+            />
+          )}
+        </div>
+
+        <div>
+          <h2>Consume</h2>
+        </div>
+      </main>
     </>
   )
 }
---
+++ tip-calculator/src/components/MenuItem.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MenuItem(): React.JSX.Element {
+    return <div>
+
+    </div>
+}
